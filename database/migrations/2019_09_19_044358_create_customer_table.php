63feffd56bc306f7d75404f49b33f291d35d455c
LoryLeticee <loryleticee@gmail.com>
2019-09-19 07:02:34 +0200

    fix ok , all table created

--- database/migrations/2019_09_19_044358_create_customer_table.php
+++ database/migrations/2019_09_19_044358_create_customer_table.php
@@ -13,22 +13,19 @@ class CreateCustomerTable extends Migration
      */
     public function up()
     {
-        if (Schema::hasTable('customer'))
-        {
-            Schema::create('customer', function (Blueprint $table) {
-                $table->bigIncrements('id');
-                $table->string('firstname', 100);
-                $table->string('lastname', 100);
-                $table->string('adress', 255);
-                $table->integer('zip_code');
-                $table->string('city', 255);
-                $table->string('country', 100);
-                $table->string('driving_licence', 255);
-                $table->enum('role',['admin','anonym','buyer']);
-                $table->string('token', 255);
-                $table->timestamps();
-            });
-        }
+        Schema::create('customer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('firstname', 100);
+            $table->string('lastname', 100);
+            $table->string('adress', 255);
+            $table->integer('zip_code');
+            $table->string('city', 255);
+            $table->string('country', 100);
+            $table->string('driving_licence', 255);
+            $table->enum('role',['admin','anonym','buyer']);
+            $table->string('token', 255);
+            $table->timestamps();
+        });
     }
 
     /**
