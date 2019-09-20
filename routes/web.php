3c30c9bad0fc2c2c1b06d51b084b66dd8910438e
lyafmounia <lyaf.mounia@gmail.com>
2019-09-20 14:21:03 +0200

    Auth controller

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/vehicle', 'VehicleController@index');
 Route::get('/customer', 'CustomerController@index');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
